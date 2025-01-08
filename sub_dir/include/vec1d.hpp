d68ec962ee30a4956c297cb01b5a4425fa9b8a46
kenichi.hoshi <hosi5678@gmai.com>
2025-01-08 16:56:36 +0900

    ・vec1d,vec2dの加減乗除のメンバ関数を追加した。

---
+++ sub_dir/include/vec1d.hpp
@@ -0,0 +1,234 @@
+#ifndef B9B8D291_40EB_4F6C_BA53_03E3CEF4EED3
+#define B9B8D291_40EB_4F6C_BA53_03E3CEF4EED3
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream> // fileの書き込みに使用する
+#include <iomanip>  // fileの書き込みに使用する
+#include <algorithm>
+
+#include <stdexcept>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+class vec1d {
+
+  public:
+    int length;
+    std::vector<uint8_t> vec;
+
+    // default constructor
+    vec1d():length(0),vec(){}
+
+    // constructor with argument
+    vec1d(int _length): length(_length),vec(_length,0) {}
+
+    // copy constructor
+      vec1d(const vec1d& obj): length(obj.length),vec(obj.vec) {}
+
+      // destructor
+      ~vec1d() = default;
+
+    // constructor with vector
+    vec1d(std::vector<uint8_t> obj):vec1d(obj.size()) {
+
+       if(obj.empty()){
+          throw std::runtime_error("obj is empty.");
+       }
+
+       for(size_t i=0; i<obj.size(); i++) {
+          this->vec[i]=obj[i];
+       }
+
+       (*this).length=obj.size();
+
+    }
+
+    // operator=() vectorによる代入
+    vec1d& operator=(const std::vector<uint8_t>& obj) {
+
+       if(this->length!=obj.size()) {
+          throw std::runtime_error("size is different.");
+       }
+
+       this->vec=obj;
+       this->length=obj.size();
+       return *this;
+    }
+
+vec1d& operator=(const vec1d& obj) {
+    if (this != &obj) {
+        this->length = obj.length;
+        this->vec = obj.vec;
+    }
+    return *this;
+}
+
+
+    // 255を超える値を255にする(参照を渡す)
+   void clip(uint8_t& d) {
+      if(d>255) d=255;
+   }
+
+   // 255を超える値を255にする(参照を渡さない)
+   uint8_t clip(uint8_t d) const {
+      return (d > 255) ? 255 : d;
+   }
+
+    // 全要素にdを乗算
+    void mul(uint8_t d) {
+
+       for(size_t i=0; i<this->length; i++) {
+         this->vec[i]*=d;
+
+         clip(this->vec[i]);
+
+      }
+
+   }
+
+   // 全要素にdを除算
+   void div(uint8_t d) {
+      if (d==0) {
+         throw std::runtime_error("(in div function):division by zero.");
+      }
+
+      for(size_t i=0; i<this->length; i++) {
+         this->vec[i]/=d;
+      }
+   }
+
+    // 全要素でdを除算(friend function)
+      friend vec1d operator/(uint8_t d,const vec1d& obj) {
+         vec1d ret(obj.length);
+
+         for(size_t i=0; i<obj.length; i++) {
+            if(obj.vec[i]==0) {
+               throw std::runtime_error("(in operator d/): division by zero.");
+            }
+            ret.vec[i]=d/obj.vec[i];
+         }
+
+         return ret;
+      }
+
+    // 全要素にdを加算
+   void add(uint8_t d) {
+      for(size_t i=0; i<this->length; i++) {
+         this->vec[i]+=d;
+         clip(this->vec[i]);
+      }
+   }
+
+    // 全要素にdを減算(friend function)
+      friend vec1d operator-(uint8_t d,const vec1d& obj) {
+         vec1d ret(obj.length);
+         for(size_t i=0; i<obj.length; i++) {
+            ret.vec[i] = (d >= obj.vec[i]) ? d - obj.vec[i] : 0;
+         }
+
+         return ret;
+      }
+
+    // vector同士の乗算(friend function)
+      friend vec1d operator*(const vec1d& obj1,const vec1d& obj2) {
+         if(obj1.length!=obj2.length) {
+            throw std::runtime_error("(in operator*): size is different.");
+         }
+
+         vec1d ret(obj1.length);
+
+         for(size_t i=0; i<obj1.length; i++) {
+            ret.vec[i] = static_cast<uint8_t>(std::min<int>(obj1.vec[i] * obj2.vec[i], 255));
+         }
+
+      return ret;
+
+   }
+    // vector同士の減算
+    vec1d operator-(const vec1d& obj) {
+       if(this->length!=obj.length) {
+          throw std::runtime_error("(in operator-):size is different.");
+       }
+
+       vec1d ret(this->length);
+
+       for(size_t i=0; i<this->length; i++) {
+          ret.vec[i]=this->vec[i]-obj.vec[i];
+
+          if(ret.vec[i]<0) {
+             ret.vec[i]=0;
+          }
+
+       }
+       return ret;
+
+      }
+
+    // directoryの作成
+   void createDir(const std::string& dir) {
+
+    struct stat info;
+
+    if (stat(dir.c_str(), &info) != 0) { // ディレクトリが存在しない
+         if (mkdir(dir.c_str(), 0755) != 0) {
+            throw std::runtime_error("Failed to create directory: " + dir);
+         }
+
+    } else if (!(info.st_mode & S_IFDIR)) { // 存在するがディレクトリでない
+      throw std::runtime_error(dir + " exists but is not a directory.");
+   }
+}
+
+// vectorの内容を10進でファイルに書き込む
+void createFile(const int precision,const std::string& dir,const std::string& file_name) {
+
+   createDir(dir);
+
+   std::ofstream ofs(dir+"/"+file_name);
+
+   if(!ofs) {
+      throw std::runtime_error("(in createFile):can not open file.");
+   }
+
+   for(size_t i=0; i<this->length; i++) {
+      ofs << "[" << i << "]: " << std::setw(precision) << this->vec[i] << std::endl;
+   }
+
+   ofs.close();
+}
+
+// vectorの内容を16進でファイルに書き込む
+void createFileHex(const std::string& dir,const std::string& file_name) {
+
+   createDir(dir);
+
+   std::ofstream ofs(dir+"/"+file_name);
+
+   if(!ofs) {
+      throw std::runtime_error("(in createFileHex):can not open file.");
+   }
+
+   for(size_t i=0; i<this->length; i++) {
+      ofs <<"[" << i << "]: " << std::hex << std::setw(2) << static_cast<int>(this->vec[i]) << std::endl;
+   }
+
+   ofs.close();
+}
+
+// vectorの内容を表示する
+void show() {
+   for(size_t i=0; i<this->length; i++) {
+      std::cout <<"[ " << i << "]: " << static_cast<int>(this->vec[i]) << std::endl;
+   }
+
+}
+
+
+};// closing brace for class vec1d
+
+#endif /* B9B8D291_40EB_4F6C_BA53_03E3CEF4EED3 */
--- sub_dir/include/vec2d.hpp
+++ sub_dir/include/vec2d.hpp
@@ -7,7 +7,6 @@
 #include <sstream>
 #include <fstream> // fileの書き込みに使用する
 #include <iomanip>  // fileの書き込みに使用する
-#include <string>
 #include <algorithm>
 
 #include <stdexcept>
@@ -15,27 +14,32 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include "vec1d.hpp"
+
 class vec2d {
 
     public:
       int ylength;
       int xlength;
-      int depth;
       std::stringstream file_name;
-      
+
+      // uint8_t型の2次元vector
       std::vector<std::vector<uint8_t>> vec;
 
       // default constructor
-      vec2d(){}
+      vec2d():ylength(0),xlength(0),vec(){}
 
       // constructor with argument
       vec2d(int _ylength, int _xlength): ylength(_ylength),xlength(_xlength),vec(_ylength,std::vector<u_int8_t>(_xlength,0)) {}
+
+      // copy constructor
+      vec2d(const vec2d& obj): ylength(obj.ylength),xlength(obj.xlength),vec(obj.vec) {}
 
       // constructor with vector
       vec2d(std::vector<std::vector<uint8_t>> obj):vec2d(obj.size(),obj[0].size()) {
 
          if(obj.empty()){
-            throw std::runtime_error("obj is empty.");
+            throw std::runtime_error("(in constructor with vector):obj is empty.");
          }
 
          for(size_t j=0; j<obj.size(); j++) {
@@ -49,7 +53,277 @@ class vec2d {
 
       }
 
-      // constructor with 1D vector.
+   // operator=(vector) vectorによる代入
+   vec2d& operator=(const std::vector<std::vector<uint8_t>>& obj) {
+      if(this->ylength!=obj.size() || this->xlength!=obj[0].size()) {
+         throw std::runtime_error("(in operator=vector):size is different.");
+      }
+
+      this->vec=obj;
+      this->ylength=obj.size();
+      this->xlength=obj[0].size();
+
+      return *this;
+   }
+
+   // operator=(vec2d) vec2dによる代入
+   vec2d& operator=(const vec2d& obj) {
+      if(this!=&obj) {
+         this->vec=obj.vec;
+         this->ylength=obj.ylength;
+         this->xlength=obj.xlength;
+      }
+      return *this;
+   }
+
+   // 255を超える値を255にする(参照を渡す)
+   void clip(uint8_t& d) {
+      if(d>255) d=255;
+   }
+
+      // 255を超える値を255にする(参照を渡さない)
+   uint8_t clip(uint8_t d) const {
+      return (d > 255) ? 255 : d;
+   }
+
+
+   // vec1dのオブジェクトをoperator=(vector 1d)の形式でvec2dに変換
+   vec2d& operator=(const vec1d& obj) {
+      if(this->ylength*this->xlength!=obj.length) {
+         throw std::runtime_error("(in operator=(vec1d) ):size is different.");
+      }
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            this->vec[j][i]=obj.vec[j*this->xlength+i];
+         }
+      }
+
+      return *this;
+   }
+
+   // vec2dのオブジェクトをoperator=(vec2d)の形式でvec1dに変換
+   vec1d to_vec1d(const vec2d& obj) const{
+      vec1d ret(obj.ylength*obj.xlength);
+
+      for(int j=0; j<obj.ylength; j++) {
+         for(int i=0; i<obj.xlength; i++) {
+            ret.vec[j*obj.xlength+i]=obj.vec[j][i];
+         }
+      }
+
+      return ret;
+   }
+
+   // vec2dのオブジェクト同士の加算
+   vec2d operator+(const vec2d& obj) {
+
+      if(this->ylength!=obj.ylength || this->xlength!=obj.xlength) {
+         throw std::runtime_error("(in operator+):size is different.");
+      }
+
+      vec2d ret(this->ylength,this->xlength);
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            ret.vec[j][i]=this->vec[j][i]+obj.vec[j][i];
+            clip(ret.vec[j][i]);
+         }
+      }
+
+      return ret;
+   }
+
+   // vec2dのオブジェクト同士の減算
+   vec2d operator-(const vec2d& obj) {
+
+      if(this->ylength!=obj.ylength || this->xlength!=obj.xlength) {
+         throw std::runtime_error("(in operator-):size is different.");
+      }
+
+      vec2d ret(this->ylength,this->xlength);
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            ret.vec[j][i]=this->vec[j][i]-obj.vec[j][i];
+
+            if(ret.vec[j][i]<0) {
+               ret.vec[j][i]=0;
+            }
+         }
+      }
+
+      return ret;
+   }
+
+   // vec2dのオブジェクトとuint8_t型のオブジェクトの加算
+   vec2d operator+(uint8_t d) {
+      vec2d ret(this->ylength,this->xlength);
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            ret.vec[j][i]=this->vec[j][i]+d;
+            clip(ret.vec[j][i]);
+         }
+      }
+
+      return ret;
+   }
+
+   // vec2dのオブジェクトとuint8_t型のオブジェクトの減算
+   vec2d operator-(uint8_t d) {
+      vec2d ret(this->ylength,this->xlength);
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            ret.vec[j][i]=(d>=this->vec[j][i]) ? d-this->vec[j][i] : 0;
+         }
+      }
+
+      return ret;
+   }
+
+   // vec2dのオブジェクトとuint8_t型のオブジェクトの乗算
+   vec2d operator*(uint8_t d) {
+      vec2d ret(this->ylength,this->xlength);
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            ret.vec[j][i]=static_cast<uint8_t>(std::min<int>(this->vec[j][i]*d,255));
+         }
+      }
+
+      return ret;
+   }
+
+   // vec2dのオブジェクトとuint8_t型のオブジェクトの除算
+   vec2d operator/(uint8_t d) {
+      vec2d ret(this->ylength,this->xlength);
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            ret.vec[j][i]=d/this->vec[j][i];
+         }
+      }
+
+      return ret;
+   }
+
+   // 全要素にdを加算
+   void add(uint8_t d) {
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            this->vec[j][i]+=d;
+            clip(this->vec[j][i]);
+         }
+      }
+   }
+
+   // 全要素にdを減算
+   void sub(uint8_t d) {
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            this->vec[j][i]-=d;
+            if(this->vec[j][i]<0) {
+               this->vec[j][i]=0;
+            }
+         }
+      }
+   }
+
+   // 全要素にdを乗算
+   void mul(uint8_t d) {
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            this->vec[j][i]=static_cast<uint8_t>(std::min<int>(this->vec[j][i]*d,255));
+         }
+      }
+   }
+
+   // 全要素にdを除算
+   void div(uint8_t d) {
+
+
+      for(int j=0; j<this->ylength; j++) {
+         for(int i=0; i<this->xlength; i++) {
+            if(this->vec[j][i]==0) {
+               throw std::runtime_error("(in div d/ function):division by zero.");
+            }
+
+            this->vec[j][i]=d/this->vec[j][i];
+         }
+      }
+   }
+
+// directoryの作成
+void createDir(const std::string& dir) {
+
+    struct stat info;
+
+    if (stat(dir.c_str(), &info) != 0) { // ディレクトリが存在しない
+         if (mkdir(dir.c_str(), 0755) != 0) {
+            throw std::runtime_error("Failed to create directory: " + dir);
+         }
+
+    } else if (!(info.st_mode & S_IFDIR)) { // 存在するがディレクトリでない
+      throw std::runtime_error(dir + " exists but is not a directory.");
+   }
+}
+
+
+// vec2dの内容を10進で1列にファイルに書き込む
+void createFile(const int precision,const std::string& dir,const std::string& file_name) {
+
+   createDir(dir);
+
+   std::ofstream ofs(dir+"/"+file_name);
+
+   if(!ofs) {
+      throw std::runtime_error("(in createFile):can not open file.");
+   }
+
+   for (size_t j=0; j<this->ylength; j++) {
+      for(size_t i=0; i<this->xlength; i++) {
+         ofs << "[" << i << "]: " << std::setw(precision) << this->vec[j][i] << std::endl;
+      }
+   }
+      // ファイルを閉じる
+      ofs.close();
+
+}
+
+// vec2dの内容を16進で1列にファイルに書き込む
+void createFileHex(const std::string& dir,const std::string& file_name) {
+
+   createDir(dir);
+
+   std::ofstream ofs(dir+"/"+file_name);
+
+   if(!ofs) {
+      throw std::runtime_error("(in createFileHex):can not open file.");
+   }
+
+   for (size_t j=0; j<this->ylength; j++) {
+      for(size_t i=0; i<this->xlength; i++) {
+         ofs << "[" << i << "]: " << std::hex << std::setw(2) << static_cast<int>(this->vec[j][i]) << std::endl;
+      }
+   }
+
+   // ファイルを閉じる
+   ofs.close();
+}
+
+// vec2dの内容を表示する。
+void show() {
+   for (size_t j=0; j<this->ylength; j++) {
+      for(size_t i=0; i<this->xlength; i++) {
+         std::cout << "[" << i << "]: " << static_cast<int>(this->vec[j][i]) << std::endl;
+      }
+   }
+}
+
+// destructor
+~vec2d() = default;
 
 
 };
